42ab62671cf8d453e6f4f0c5bdb1f8f278c147ce
Alexey <61292219+Ocvcorp@users.noreply.github.com>
2020-09-10 16:58:53 +0300

    Добавлены комментарии

--- VacationTimeConsole/Program.cs
+++ VacationTimeConsole/Program.cs
@@ -11,6 +11,7 @@ namespace VacationTimeConsole
     {
         static void Main(string[] args)
         {
+            //Конечные даты
             DateTime firstDay;
             DateTime finishDay;
 
@@ -18,12 +19,12 @@ namespace VacationTimeConsole
 
             if (FormattedDate("first",out firstDay) && FormattedDate("finish", out finishDay))
             {
-                if (firstDay<finishDay)
+                if (firstDay<=finishDay)//если даты введены последовательно
                 {
                     Console.WriteLine("Your vacation period will be {0} day(s)",
                                         finishDay.Subtract(firstDay).Days);
                 }
-                else
+                else//если вводимый первый день оказался по дате позже последнего дня
                 {
                     Console.WriteLine("Chronological mistake. Last day can't be earlier than first day");
                 }               
@@ -34,7 +35,7 @@ namespace VacationTimeConsole
             Console.ReadKey();
         }
         
-        static bool FormattedDate(string dayPoint, out DateTime date)
+        static bool FormattedDate(string dayPoint, out DateTime date)// функция проверки формата даты и извлечения даты из строки
         {
             bool isCorrectFormat=false;
             string someString;
@@ -43,13 +44,13 @@ namespace VacationTimeConsole
             {
                 Console.WriteLine("Input a "+dayPoint+" day date like dd:mm:yyyy"); 
                 someString = Console.ReadLine();
-                Regex dateFormat = new Regex(@"(\w{2}).(\w{2}).(\w{4})");
+                Regex dateFormat = new Regex(@"(\w{2}).(\w{2}).(\w{4})");//регулярные выражения
                 MatchCollection matchDateFormat = dateFormat.Matches(someString);
                 if (matchDateFormat.Count > 0)
                 {
                     if (matchDateFormat[0].Groups.Count == 4)
                     {
-                        try
+                        try//разбор строки
                         {
                             date = new DateTime(Int32.Parse(matchDateFormat[0].Groups[3].Value),
                                                 Int32.Parse(matchDateFormat[0].Groups[2].Value),
@@ -57,15 +58,15 @@ namespace VacationTimeConsole
                             isCorrectFormat = true;
                             break;
                         }
-                        catch (ArgumentOutOfRangeException e)
+                        catch (ArgumentOutOfRangeException e)//исключение если месяцы и номер дня не соответствуют календарю
                         {
                             Console.WriteLine(e.Message);
                         }
                     }
                 }
-                date = DateTime.Now;
+                date = DateTime.Now;//"резервное" присвоение для параметра out
                 Console.WriteLine("Incorrect Date or Date Format. To try again press any key, or 'Esc' to quit");
-                key=Console.ReadKey().Key;
+                key=Console.ReadKey().Key;//считывание решения пользователя
             } while (key != ConsoleKey.Escape);
             return isCorrectFormat;
         }
